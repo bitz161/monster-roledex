c9b3665a802141e03aad7a4e6a9b9083156bde9f
bitz161 <bitzgarcia1996@gmail.com>
2023-06-02 09:52:00 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -1,15 +1,34 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.scss";
 import CardList from "./components/cardlist/cardlist.component";
 import SearchBox from "./components/searchbox/searchbox.component";
 
 const App = () => {
   const [searchField, setSearchField] = useState("");
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters);
+  const [stringField, setStringField] = useState("");
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => {
+        setMonsters(users);
+      });
+  }, []);
+
+  useEffect(() => {
+    const newFilteredMonster = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+    setFilteredMonsters(newFilteredMonster);
+  }, [monsters, searchField]);
 
   const onSearchChange = (event) => {
-    const searchField = event.target.value.toLocaleLowerCase();
-    setSearchField(searchField);
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    setSearchField(searchFieldString);
   };
+
   return (
     <div className="App">
       <h1>Monster Rolodex</h1>
@@ -17,7 +36,7 @@ const App = () => {
         onChangeHandler={onSearchChange}
         placeholder="search monsters"
       />
-      <CardList />
+      <CardList monsters={filteredMonsters} />
     </div>
   );
 };
--- src/components/cardlist/cardlist.component.js
+++ src/components/cardlist/cardlist.component.js
@@ -1,6 +1,7 @@
 import "./cardlist.style.scss";
 
-const CardList = () => {
+const CardList = ({ monsters }) => {
+  console.log(monsters);
   return <div>Working</div>;
 };
 
